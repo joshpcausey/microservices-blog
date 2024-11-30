48f2364fa325f098c64ada4725d6e62e7a6d2381
Joshua Causey <joshpcausey@gmail.com>
2024-11-30 17:59:51 -0600

    add comment moderation

--- query/index.js
+++ query/index.js
@@ -19,13 +19,24 @@ app.post('/events', (req, res) => {
     const { id, title } = data;
     posts[id] = { id, title, comments: [] };
   }
+
   if (type === 'CommentCreated') {
-    const { id, content, postId } = data;
+    const { id, content, postId, status } = data;
     const post = posts[postId];
-    post.comments.push({ id, content });
+    post.comments.push({ id, content, status });
   }
 
-  console.log(posts);
+  if (type === 'CommentUpdated') {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => {
+      return comment.id === id;
+    });
+
+    comment.content = content;
+    comment.status = status;
+  }
 
   res.send({});
 });
