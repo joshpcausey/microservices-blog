4283139d7798ff7ce4036f4e3f39622df3f3fd67
Joshua Causey <joshpcausey@gmail.com>
2024-11-30 08:01:56 -0600

    fixed event bus

--- comments/index.js
+++ comments/index.js
@@ -23,18 +23,14 @@ app.post('/posts/:id/comments', async (req, res) => {
 
   commentsByPostId[req.params.id] = comments;
 
-  axios
-    .post('http://localhost:4005/events', {
-      type: 'CommentCreated',
-      data: {
-        id: commentId,
-        content,
-        postId: req.params.id,
-      },
-    })
-    .catch((err) => {
-      console.error(err.message);
-    });
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+    },
+  });
 
   res.status(201).send(comments);
 });
