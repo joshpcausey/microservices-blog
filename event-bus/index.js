7f9f9cce83dca3708f49b085f636d7a99bb0fa78
Joshua Causey <joshpcausey@gmail.com>
2024-11-30 19:29:38 -0600

    add event syncing

--- event-bus/index.js
+++ event-bus/index.js
@@ -7,14 +7,30 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+const events = [];
+
 app.post('/events', (req, res) => {
   const event = req.body;
 
-  axios.post('http://localhost:4000/events', event);
-  axios.post('http://localhost:4001/events', event);
-  axios.post('http://localhost:4002/events', event);
-  axios.post('http://localhost:4003/events', event);
+  events.push(event);
+
+  axios.post('http://localhost:4000/events', event).catch((err) => {
+    console.log(err);
+  });
+  axios.post('http://localhost:4001/events', event).catch((err) => {
+    console.log(err);
+  });
+  axios.post('http://localhost:4002/events', event).catch((err) => {
+    console.log(err);
+  });
+  axios.post('http://localhost:4003/events', event).catch((err) => {
+    console.log(err);
+  });
   res.send({ status: 'OK' });
+});
+
+app.get('/events', (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
