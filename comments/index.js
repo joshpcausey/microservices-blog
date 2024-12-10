5e8010a958e735316a5d45f420d63cb922568796
Joshua Causey <joshpcausey@gmail.com>
2024-12-10 11:54:23 -0600

    add deployments for all services and set up ingress

--- comments/index.js
+++ comments/index.js
@@ -23,7 +23,7 @@ app.post('/posts/:id/comments', async (req, res) => {
 
   commentsByPostId[req.params.id] = comments;
 
-  await axios.post('http://localhost:4005/events', {
+  await axios.post('http://event-bus-srv:4005/events', {
     type: 'CommentCreated',
     data: {
       id: commentId,
@@ -50,7 +50,7 @@ app.post('/events', async (req, res) => {
     comment.status = status;
 
     await axios
-      .post('http://localhost:4005/events', {
+      .post('http://event-bus-srv:4005/events', {
         type: 'CommentUpdated',
         data: {
           id,
@@ -67,6 +67,6 @@ app.post('/events', async (req, res) => {
   res.send({});
 });
 
-app.listen('4001', () => {
+app.listen(4001, () => {
   console.log('comments listening on port 4001');
 });
--- query/index.js
+++ query/index.js
@@ -48,9 +48,11 @@ app.post('/events', (req, res) => {
 app.listen(4002, async () => {
   console.log('Query listening on port 4002');
 
-  const res = await axios.get('http://localhost:4005/events').catch((err) => {
-    console.log(err.message);
-  });
+  const res = await axios
+    .get('http://event-bus-srv:4005/events')
+    .catch((err) => {
+      console.log(err.message);
+    });
 
   for (let event of res.data) {
     console.log('processing event', event.type);
